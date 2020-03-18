782def29021a8a807376e75ba37af40ed7c042cd
Amirah Almani <amirahalmani@gmail.com>
2020-03-18 16:31:08 +0300

    Updates

---
+++ precache-manifest.b1a1ee5a5d4fac76fd744896ee909e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690569e670318aa5c4080e2dc52b5137",
+    "url": "/CarRent-Front-End/index.html"
+  },
+  {
+    "revision": "29aba9c668d125636111",
+    "url": "/CarRent-Front-End/static/css/main.2d209ab9.chunk.css"
+  },
+  {
+    "revision": "abec3d859422d23cd1a4",
+    "url": "/CarRent-Front-End/static/js/2.53eebb8a.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/CarRent-Front-End/static/js/2.53eebb8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29aba9c668d125636111",
+    "url": "/CarRent-Front-End/static/js/main.f01a2e9d.chunk.js"
+  },
+  {
+    "revision": "cc7f030d9a868fc2a557",
+    "url": "/CarRent-Front-End/static/js/runtime-main.5910c189.js"
+  }
+]);
