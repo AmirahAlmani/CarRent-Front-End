adb4b82722159d1a58932f26be06b452142837d4
Amirah Almani <amirahalmani@gmail.com>
2020-03-18 16:13:43 +0300

    heroku

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,7 +2,7 @@ let apiURL;
 const expressPort = 5000;
 const apiUrls = {
   development: `http://localhost:${expressPort}/api`,
-  production: `https://aqueous-atoll-85096.herokuapp.com/api`
+  production: `https://nameless-journey-52364.herokuapp.com`
 };
 if (window.location.hostname === "localhost") {
   apiURL = apiUrls.development;
